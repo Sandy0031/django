19e97b2dd2c26ef5976b5d951946c676f866d932
Sandy <kumarsndp03@gmail.com>
2022-10-08 20:38:08 +0530

    other pages has been added to the app

--- home/views.py
+++ home/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Hello there")
+
+def about(request):
+    return HttpResponse("this is about page")
+
+def service(request):
+    return HttpResponse("this is service page")
+
+def contact(request):
+    return HttpResponse("this is contact page")
